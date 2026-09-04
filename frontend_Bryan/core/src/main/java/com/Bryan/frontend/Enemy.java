1f51e397205710aadcce4a3da6bdfb884d3f328a
unknown <menfelixchicken2007@gmail.com>
2026-09-04 11:00:58 +0700

    CS1 - added player and enemy (still working on it)

--- frontend_Bryan/core/src/main/java/com/Bryan/frontend/Enemy.java
+++ frontend_Bryan/core/src/main/java/com/Bryan/frontend/Enemy.java
@@ -5,9 +5,40 @@ public class Enemy {
     public int hp;
     public int maxHp;
 
-    public Enemy(String name, int hp){
-        this.name=name;
-        this.hp=hp;
-        this.maxHp=hp;
+    public Enemy(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        this.hp = this.hp - damage;
+        // 2. HP must not go below 0.
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        // 3. Display the current HP in the format: [EnemyName] took [damage] damage! HP: [currentHP]/[maxHP]
+        System.out.println(this.name + " took " + damage + "damage! HP:" + this.hp + "/" + this.maxHp);
+        // 4. If HP reaches 0, display that the Enemy has been defeated, in the format: [EnemyName] was defeated!
+        if (this.hp = 0) {
+            System.out.println(this.name + " has been defeated!");
+        }
+    }
+
+    public void attack(Player player, int damage) {
+        // 1. Display information that the Enemy is attacking the Player, in the format: [EnemyName] unleashes bullet barrage on [PlayerName]!
+        System.out.println(this.name + " unleashes bullet barrage on " + player + "!");
+        // 2. Call the Player's takeDamage() method using the given damage.
+        takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if (this.hp > 0) {
+            true;
+        } else if (this.hp <= 0) {
+            false;
+        }
     }
 }
--- frontend_Bryan/core/src/main/java/com/Bryan/frontend/Player.java
+++ frontend_Bryan/core/src/main/java/com/Bryan/frontend/Player.java
@@ -6,10 +6,46 @@ public class Player {
     public int power;
     public int spellCards;
 
-    public Player(String name, int hp, int power, int spellCards){
-        this.name=name;
-        this.hp=hp;
-        this.power=power;
-        this.spellCards=spellCards;
+    public Player(String name, int hp, int power, int spellCards) {
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.spellCards = spellCards;
+    }
+
+
+    public void takeDamage(int damage) {
+        // 1. Reduce hp by the damage value.
+        this.hp = this.hp - damage;
+        // 2. HP must not become negative.
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        // 3. If HP is still greater than 0, display the remaining HP in the format: [PlayerName] took [damage] damage! Remaining HP: [hp]
+        if (this.hp > 0) {
+            System.out.println(this.name + " took " + damage + " damage! remaining HP: " + this.hp);
+        }
+        // 4. If HP reaches 0, display a message that the Player has been defeated.
+        else if (this.hp = 0) {
+            System.out.println(this.name + "has been defeated");
+        }
+    }
+
+    public void shoot(Enemy target) {
+        // 1. Create an int named damage, calculated by adding 10 to power.
+        int damage = 10 + this.power;
+        // 2. Display information that the Player is shooting the Enemy, in the format: [name] shoots [TargetName] dealing [damage] DMG!
+        System.out.println(this.name + " shoots " + target + " deals " + damage + " DMG ");
+        // 3. Call the Enemy object's takeDamage() method.
+        target.takeDamage(damage);
+    }
+
+    public boolean isAlive() {
+        // 1. Return true if hp > 0, and false otherwise
+        if (hp > 0) {
+            true;
+        } else if (hp <= 0) {
+            false;
+        }
     }
 }
