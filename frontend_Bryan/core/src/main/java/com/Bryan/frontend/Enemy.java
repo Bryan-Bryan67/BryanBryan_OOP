16dc27be7975542c7ccfe72624bee5f35da87647
unknown <menfelixchicken2007@gmail.com>
2026-09-06 15:52:40 +0700

    fixed the code by fixing the player class and enemy class

--- frontend_Bryan/core/src/main/java/com/Bryan/frontend/Enemy.java
+++ frontend_Bryan/core/src/main/java/com/Bryan/frontend/Enemy.java
@@ -8,37 +8,36 @@ public class Enemy {
     public Enemy(String name, int hp) {
         this.name = name;
         this.hp = hp;
+        this.maxHp= hp;
     }
 
 
     public void takeDamage(int damage) {
         // 1. Reduce hp by the damage value.
-        this.hp = this.hp - damage;
+        this.hp -=damage;
         // 2. HP must not go below 0.
         if (this.hp < 0) {
             this.hp = 0;
         }
         // 3. Display the current HP in the format: [EnemyName] took [damage] damage! HP: [currentHP]/[maxHP]
-        System.out.println(this.name + " took " + damage + "damage! HP:" + this.hp + "/" + this.maxHp);
+        if (this.hp>0) {
+            System.out.println(name + " took " + damage + " damage! HP:" + this.hp + "/" + this.maxHp);
+        }
         // 4. If HP reaches 0, display that the Enemy has been defeated, in the format: [EnemyName] was defeated!
-        if (this.hp = 0) {
-            System.out.println(this.name + " has been defeated!");
+        if(this.hp==0){
+            System.out.println(name + " has been defeated!");
         }
     }
 
     public void attack(Player player, int damage) {
         // 1. Display information that the Enemy is attacking the Player, in the format: [EnemyName] unleashes bullet barrage on [PlayerName]!
-        System.out.println(this.name + " unleashes bullet barrage on " + player + "!");
+        System.out.println(name + " unleashes bullet barrage on " + player.name + "!");
         // 2. Call the Player's takeDamage() method using the given damage.
-        takeDamage(damage);
+        player.takeDamage(damage);
     }
 
     public boolean isAlive() {
         // 1. Return true if hp > 0, and false otherwise
-        if (this.hp > 0) {
-            true;
-        } else if (this.hp <= 0) {
-            false;
-        }
+      return this.hp > 0;
     }
 }
