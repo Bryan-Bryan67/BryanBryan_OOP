ba15d0e621e36ab2beaaf3137a1382c04434dcc1
unknown <menfelixchicken2007@gmail.com>
2026-09-15 11:37:20 +0700

    AA2 - fixed the constructor on fairy and boss and then instatiated player, fairy, boss, and item

--- frontend_Bryan/core/src/main/java/com/Bryan/frontend/Boss.java
+++ frontend_Bryan/core/src/main/java/com/Bryan/frontend/Boss.java
@@ -5,18 +5,18 @@ import com.badlogic.gdx.graphics.Color;
 public class Boss extends Enemy{
 //Pre-CS2 asnwer: I think the type of inheritence is multilevel inheritence since it inherits from GameObject to Enemy and then to Boss
     public Boss(String name, int hp) {
-        super(380, 400, 48, 48,0, Color.BLUE);
+        super(380, 400, 48, 48,0,name, hp, Color.BLUE);
         this.name = name;
         this.hp = hp;
         this.maxHp= hp;
-        this.scoreValue=100;
+        this.scoreValue=5000L;
     }
 
     public Boss(float x, float y, String name, int hp) {
-        super(x, y, 48, 48,0,Color.BLUE);
+        super(x, y, 48, 48,Color.BLUE, name,hp,5000L);
         this.name = name;
         this.hp = hp;
         this.maxHp= hp;
-        this.scoreValue=100;
+        this.scoreValue=5000L;
     }
 }
--- frontend_Bryan/core/src/main/java/com/Bryan/frontend/Fairy.java
+++ frontend_Bryan/core/src/main/java/com/Bryan/frontend/Fairy.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Color;
 public class Fairy extends Enemy {
     //Pre-CS2 asnwer: I think the type of inheritence is multilevel inheritence since it inherits from GameObject to Enemy and then to Fairy
     public Fairy(String name, int hp) {
-        super(150, 380, 24, 24,Color.PINK, "Fairy", ,500L);
+        super(150, 380, 24, 24,Color.PINK, name,hp ,500L);
         this.name = name;
         this.hp = hp;
         this.maxHp= hp;
@@ -13,7 +13,7 @@ public class Fairy extends Enemy {
     }
 
     public Fairy(float x, float y, String name, int hp) {
-        super(x, y, 24, 24,0,Color.PINK);
+        super(x, y, 24, 24,Color.PINK, name,hp,500L);
         this.name = name;
         this.hp = hp;
         this.maxHp= hp;
